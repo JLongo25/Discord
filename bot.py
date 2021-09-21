ba0c9721a023074af92f68a3a550deb67bbdfc77
JLongo25 <30610160+JLongo25@users.noreply.github.com>
2021-09-21 11:23:20 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,6 @@ from youtube_dl import YoutubeDL as yt
 from discord.utils import get
 from urllib.request import urlopen
 import re
-import time
 import asyncio
 
 load_dotenv()
@@ -18,8 +17,10 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='~')
 client = discord.Client()
 
-queue = {}
-duration = {}
+# Songs put into queue
+queue_list = {}
+# To embed the queue list for users
+list_queue = {}
 
 
 async def player(ctx, song_link):
@@ -37,7 +38,8 @@ async def player(ctx, song_link):
             pass
     ydl_options = {'format': 'bestaudio', 'noplaylist': 'True',
                    'postprocessors': [
-                       {'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192', }]}
+                       {'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192', }],
+                   'youtube_include_dash_manifest': False}
     ffmpeg_options = {
         'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
@@ -45,38 +47,47 @@ async def player(ctx, song_link):
         info = ydl.extract_info(song_link, download=False)
         song_link = info['formats'][0]['url']
         source = await discord.FFmpegOpusAudio.from_probe(song_link, **ffmpeg_options)
-        if ctx.message.guild.id not in queue and ctx.message.guild.id not in duration:
-            queue[ctx.message.guild.id] = []
-            duration[ctx.message.guild.id] = []
-        if len(queue[ctx.message.guild.id]) == 0 and not voice.is_playing():
+        if ctx.message.guild.id not in queue_list and ctx.message.guild.id:
+            queue_list[ctx.message.guild.id] = []
+            list_queue[ctx.message.guild.id] = []
+        if len(queue_list[ctx.message.guild.id]) == 0 and not voice.is_playing():
             voice.play(source, after=lambda e: next_song(ctx))
+            await ctx.channel.send(f"Playing: {info['title']}")
         else:
-            queue[ctx.message.guild.id].append(source)
-            duration[ctx.message.guild.id].append(info['duration'] + 10)
+            queue_list[ctx.message.guild.id].append(source)
+            list_queue[ctx.message.guild.id].append(info['title'])
             await ctx.channel.send(f"{info['title']} added to the queue.")
 
-        await asyncio.sleep(info['duration'] + 10)
-        if not voice.is_playing():
-            await voice.disconnect()
-            
+    await asyncio.sleep(info['duration'] + 180)
+    if not voice.is_playing():
+        await voice.disconnect()
+
+
+async def wait(ctx):
+    voice = get(ctx.bot.voice_clients, guild=ctx.guild)
+    await asyncio.sleep(180)
+    if voice.not_playing():
+        await voice.disconnect()
+
 
 def next_song(ctx):
     voice = get(ctx.bot.voice_clients, guild=ctx.guild)
-    try:
-        voice.play(queue[ctx.message.guild.id][0], after=lambda e: next_song(ctx))
-        del queue[ctx.message.guild.id][0]
-        time.sleep(duration[ctx.message.guild.id][0])
-        del duration[ctx.message.guild.id][0]
-    except IndexError:
-        pass
-    if not voice.is_playing():
-        asyncio.run_coroutine_threadsafe(voice.disconnect(), bot.loop)
+    if len(queue_list[ctx.message.guild.id]) > 0:
+        try:
+            voice.play(queue_list[ctx.message.guild.id][0], after=lambda e: next_song(ctx))
+            del queue_list[ctx.message.guild.id][0]
+            del list_queue[ctx.message.guild.id][0]
+        except IndexError:
+            pass
+    else:
+        if not voice.is_playing():
+            asyncio.run_coroutine_threadsafe(wait(ctx), bot.loop)
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
-
+    
 
 @bot.command()
 async def play(ctx, url):
@@ -136,23 +147,39 @@ async def skip(message):
     voice = get(bot.voice_clients, guild=message.guild)
     voice.stop()
     try:
-        del duration[message.guild.id][0]
-    except IndexError:
-        pass
-    try:
         next_song()
     except TypeError:
         pass
 
 
 @bot.command()
+async def queue(ctx):
+    y = []
+    for i, x in enumerate(list_queue[ctx.message.guild.id]):
+        z = f'{i + 1}: {x}'
+        y.append(z)
+
+    embed = discord.Embed(title='Queue', description='\n'.join(y))
+    await ctx.channel.send(embed=embed)
+
+
+@bot.command()
+async def remove(ctx, song_num: int):
+    queue_list[ctx.message.guild.id].pop(song_num - 1)
+    await ctx.channel.send(f'{list_queue[ctx.message.guild.id][song_num - 1]} has been removed.')
+    list_queue[ctx.message.guild.id].pop(song_num - 1)
+
+
+@bot.command()
 async def commands(ctx):
-    embed = discord.Embed(title='Commands', description='~play {Direct Link to Youtube} - Plays direct link from YouTube\n'\
-                                                        '~search {song name} - If more than one band has song name use band name as well\n'\
-                                                        '~skip - Skips current song and plays next in queue\n'\
-                                                        '~disconnect - Removes bot from voice channel\n'\
-                                                        '~pause - Pauses current song\n'\
-                                                        '~resume - Continues song\n'\
+    embed = discord.Embed(title='Commands', description='~play {Direct Link to Youtube} - Plays direct link from YouTube\n'
+                                                        '~search {song name} - If more than one band has song name use band name as well\n'
+                                                        '~skip - Skips current song and plays next in queue\n'
+                                                        '~queue - Displays songs in the queue\n'
+                                                        '~remove {song_number} - Removes selected song in the queue\n'
+                                                        '~disconnect - Removes bot from voice channel\n'
+                                                        '~pause - Pauses current song\n'
+                                                        '~resume - Continues song\n'
                                                         '~stop - Stops music and disconnects bot')
     await ctx.channel.send(embed=embed)
 
